1e317f9cdc3ad3046120f108ac425ec23bce463e
unknown <amanmacwan@gmail.com>
2025-03-08 23:04:25 +0530

    banner and tab

--- showcasa/src/components/Banner.jsx
+++ showcasa/src/components/Banner.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Banner = () => {
   return (
     <div className="text-center py-20">
-      <h1>Showcase Your Talent. Get Discovered.</h1>
-      <p>
-        A dynamic platform where creativity meets opportunity. Build interactive
-        profiles, share your best work, and connect with recruiters who value
-        skills over degrees.
-      </p>
+      <div className="container mx-auto px-4">
+        <div className="max-w-[700px] mx-auto">
+          <h1 className="mb-5">Showcase Your Talent. Get Discovered.</h1>
+          <p className="text-[#646167]">
+            A dynamic platform where creativity meets opportunity. Build
+            interactive profiles, share your best work, and connect with
+            recruiters who value skills over degrees.
+          </p>
+          <div className="flex flex-wrap gap-y-8 gap-x-5 items-center justify-center mt-8">
+            <Link to="/search" className="btn btn-secondary">
+              <span className="relative z-1">Discover Talent</span>
+            </Link>
+            <Link to="/login" className="btn btn-primary">
+              <span className="relative z-1">Join as Talent</span>
+            </Link>
+          </div>
+        </div>
+        <img
+          src="/banner-img.png"
+          className="h-64 object-cover md:h-auto mt-20 rounded-[20px] mx-auto block"
+        />
+      </div>
     </div>
   );
 };
--- showcasa/src/components/Header.jsx
+++ showcasa/src/components/Header.jsx
@@ -5,97 +5,213 @@ import userimg from "../assets/img/user-img.png";
 
 const Header = ({ isAuthenticated, handleLogout }) => {
   return (
-    <header className="header shadow-[0_4px_4px_rgba(222,222,222,0.25)] fixed top-0 left-0 w-full">
-        <nav className="bg-white">
-          <div className="max-w-[1420px] flex flex-wrap items-center justify-between mx-auto p-5">
-            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src={showcasa} alt="ShowCasa Logo" width={'244'} height={'38'} />
-            </a>
-           <div className="flex items-center space-x-3 md:space-x-0">
-            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
-                <span className="sr-only">Open main menu</span>
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>            
+    <header className="header shadow-[0_4px_4px_rgba(222,222,222,0.25)]">
+      <nav className="bg-white">
+        <div className="max-w-[1420px] flex flex-wrap items-center justify-between mx-auto p-5">
+          <a
+            href="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src={showcasa}
+              alt="ShowCasa Logo"
+              className="max-w-[150px] md:max-w-full"
+              width={"244"}
+              height={"38"}
+            />
+          </a>
+          <div className="flex items-center space-x-3 md:space-x-0">
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            {isAuthenticated ? (
               <div className="user-profile-img block md:hidden">
-                  <button id="dropdownDefaultButtonMobile" data-dropdown-toggle="dropdownMobile" type="button" className="flex items-center space-x-1 hover:cursor-pointer" aria-haspopup="true" aria-expanded="false">
-                  <img src={userimg} width={'56px'} height={'56px'} />
-                  <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                <button
+                  id="dropdownDefaultButtonMobile"
+                  data-dropdown-toggle="dropdownMobile"
+                  type="button"
+                  className="flex items-center space-x-1 hover:cursor-pointer"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <img src={userimg} width={"56px"} height={"56px"} />
+                  <svg
+                    class="w-2.5 h-2.5 ms-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 4 4 4-4"
+                    />
                   </svg>
                 </button>
-                <div id="dropdownMobile" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-auto">
-                    <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButtonMobile">
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Dashboard</a>
-                      </li>
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>
-                      </li>
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Earnings</a>
-                      </li>
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Sign out</a>
-                      </li>
-                    </ul>
+                <div
+                  id="dropdownMobile"
+                  class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-auto"
+                >
+                  <ul
+                    class="py-2 text-sm text-gray-700"
+                    aria-labelledby="dropdownDefaultButtonMobile"
+                  >
+                    <li>
+                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                        Dashboard
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                        Settings
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                        Earnings
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        onClick={handleLogout}
+                        class="block px-4 py-2 hover:bg-gray-100"
+                      >
+                        Sign out
+                      </a>
+                    </li>
+                  </ul>
                 </div>
-              </div>   
-           </div>
-           <div className="flex w-full md:w-auto items-center space-x-0 md:space-x-10">
-            <div className="search-input-wrapper hidden xl:block">
-              <input type="text" placeholder="Search Here" className="max-w-[320px] text-[#D0CDD3] placeholder-[#D0CDD3] px-[30px] w-100 rounded-[30px] border-[#E2E0E4] focus:outline-none focus:shadow-none focus:ring-0"/>
-            </div>
-            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-              <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
-                <li>
-                  <Link to="/search" className="font-inter text-[#211F24]">Explore</Link>                  
-                </li>
-                <li>
-                  <Link to="/" className="font-inter text-[#211F24] ">Blog</Link>        
-                </li>
-                <li>
-                  <Link to="/" className="font-inter text-[#211F24] ">Find Job</Link>        
-                </li>
-                <li>
-                  <Link to="/" className="font-inter text-[#211F24] ">Hire Talent</Link>        
-                </li>      
-                {isAuthenticated ? (
-              <div className="user-profile-img hidden md:block">
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" type="button" className="flex items-center space-x-1 hover:cursor-pointer" aria-haspopup="true" aria-expanded="false">
-                  <img src={userimg} width={'56px'} height={'56px'} />
-                  <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                  </svg>
-                </button>
-                <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-auto">
-                    <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Dashboard</a>
-                      </li>
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Settings</a>
-                      </li>
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Earnings</a>
-                      </li>
-                      <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">Sign out</a>
-                      </li>
-                    </ul>
-                </div> 
-              </div>    
+              </div>
             ) : (
               <Link to="/login">Login</Link>
             )}
-              </ul>
-            </div>   
-                 
-           </div>
-
           </div>
-                 
+          <div className="flex w-full md:w-auto items-center space-x-0 md:space-x-10">
+            <div className="search-input-wrapper hidden xl:block">
+              <input
+                type="text"
+                placeholder="Search Here"
+                className="max-w-[320px] text-[#D0CDD3] placeholder-[#D0CDD3] px-[30px] w-100 rounded-[30px] border-[#E2E0E4] focus:outline-none focus:shadow-none focus:ring-0"
+              />
+            </div>
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-default"
+            >
+              <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                <li>
+                  <Link to="/search" className="font-inter text-[#211F24]">
+                    Explore
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="font-inter text-[#211F24] ">
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="font-inter text-[#211F24] ">
+                    Find Job
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="font-inter text-[#211F24] ">
+                    Hire Talent
+                  </Link>
+                </li>
+                {isAuthenticated ? (
+                  <div className="user-profile-img hidden md:block">
+                    <button
+                      id="dropdownDefaultButton"
+                      data-dropdown-toggle="dropdown"
+                      type="button"
+                      className="flex items-center space-x-1 hover:cursor-pointer"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <img src={userimg} width={"56px"} height={"56px"} />
+                      <svg
+                        class="w-2.5 h-2.5 ms-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 10 6"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="m1 1 4 4 4-4"
+                        />
+                      </svg>
+                    </button>
+                    <div
+                      id="dropdown"
+                      class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-auto"
+                    >
+                      <ul
+                        class="py-2 text-sm text-gray-700"
+                        aria-labelledby="dropdownDefaultButton"
+                      >
+                        <li>
+                          <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                            Dashboard
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                            Settings
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                            Earnings
+                          </a>
+                        </li>
+                        <li>
+                          <Link
+                            // to="/logout"
+                            onClick={handleLogout}
+                            className="block px-4 py-2 hover:bg-gray-100"
+                          >
+                            Sign out
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                ) : (
+                  <Link to="/login">Login</Link>
+                )}
+              </ul>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
   );
